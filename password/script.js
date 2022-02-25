3f73ecc9c9f6d70d4d55690bb74fdec02b0aab3b
Cole Callaway <nccallaway54@gmail.com>
2022-02-25 15:38:02 -0700

    broken password

--- password/script.js
+++ password/script.js
@@ -1,26 +1,34 @@
-const name = document.getElementById('name')
-const password = document.getElementById('password')
-const form = document.getElementById('form')
-const errorElement = document.getElementById('error')
 
-form.addEventListener('submit', (e) => {
-    let messages = []
-    if (name.value === '' || name.value === null) {
-        messages.push('name is required')
-    }
+    const name = document.getElementsById('name')
+    const password = document.getElementById('password')
+    const form = document.getElementById('form')
+    const errorElement = document.getElementById('error')
 
-    if (password.value.length >= 10) {
-        messages.push('password must be 10 characters long')
-    }
+    form.addEventListener('submit', (e) => {
+        let messages = []
+        if (name.value === '' || name.value == null) {
+            messages.push('name is required')
+        }
 
-    if (password.value === 'password') {
-        messages.push('password cannot be password')
-    }
+        if (password.value.length >= 10) {
+            messages.push('password must be 10 characters long')
+        }
 
-    
-    if (messages.length > 0) {
-        e.preventDefault() 
-        errorElement,innerText = messages.join(', ')
-    }
-    
-})
+        if (password.value === 'password') {
+            messages.push('password cannot be password')
+        }
+
+
+        if (messages.length > 0) {
+            e.preventDefault()
+            errorElement.innerText = messages.join(', ')
+        }
+
+    })
+
+
+
+ 
+   
+
+
